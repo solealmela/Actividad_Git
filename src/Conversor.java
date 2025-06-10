d42917a2e5a4713d07941be6181d533b8153879d
Sole Pareja <solealmela1306@gmail.com>
2025-06-10 22:04:03 +0200

    Añadir mensaje en funcionalidad-extra

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,9 +1,10 @@
 public class Conversor {
     public static void main(String[] args) {
         double euros = 100;
-        double tasaCambio = 1.10; // EUR a USD
+        double tasaCambio = 1.10;
         double dolares = euros * tasaCambio;
 
         System.out.println(euros + " EUR son " + dolares + " USD");
+        System.out.println("Conversión ejecutada con éxito");
     }
 }
